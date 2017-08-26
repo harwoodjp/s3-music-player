cfd5191ff7e15a8e1c64d17ac0ac31920515ece3
Alexandre Guerra Marcondes <marcondag@gmail.com>
2017-08-26 14:01:16 -0400

    Add creating datasources

--- src/server/router.js
+++ src/server/router.js
@@ -17,12 +17,26 @@ module.exports = app => {
 	
 	app.get('/user/:id/library', async (req, res, next) => {
 		const { id: user_id } = req.params
-		const dataSources = await req.knex.select('*').from('data_source').where({ user_id })
+		const dataSources = await req.knex.select(['provider_id', 'provider_config']).from('data_source').where({ user_id })
+		// foreach data source
+		//   build the provider obj from the appropriate SDK
+		//   call the provider's getAll function
+		// res.end(all the urls)
 		res.end(JSON.stringify(dataSources))
 	})
 
-	app.post('/user/:id/providers', async (req, res, next) => {
+	app.post('/user/:id/data-source/s3', async (req, res, next) => {
+		const { id: user_id } = req.params
+		const [{ id: provider_id }] = await req.knex.select('id').from('provider').where({ name: 's3' })
+		const r = await req.knex.insert({ provider_config: JSON.stringify(req.body), user_id, provider_id }).into('data_source')
+		res.end(JSON.stringify(r))
+	})
 
+	app.get('/user/:id/data-source', async (req, res, next) => {
+		const { id: user_id } = req.params
+		const providerIds = await req.knex.select('provider_id').from('data_source').where({ user_id })
+		const providers = await req.knex.select('*').from('data_source').whereIn('provider_id', providerIds.reduce((acc, curr) => [...acc, curr.provider_id], []))
+		res.end(JSON.stringify(providers))
 	})
 
 	app.post('/user')
